5ebcc4727017273ab5fffa21bbdb1ca8447a7550
andreamaluta <andreamaluta@gmail.com>
2025-03-24 15:08:37 +0100

    [BONUS] prova del bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,22 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function timeGreeting(word){
+    const hour = new Date().getHours();
+    let message;
+    if(hour>=0 && hour<=13){
+        message = "buongiorno " + word;
+    }else if(hour>13 && hour<=17){
+        message = "buon pomeriggio " + word;
+    }else{
+        message = "buonasera " + word;
+    }
+    return message;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let timeGreet = timeGreeting(name);
+console.log(timeGreet);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
