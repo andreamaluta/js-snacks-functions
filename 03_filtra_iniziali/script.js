f8f7f5c369f72784afb25d0f7aad261c54312dbc
andreamaluta <andreamaluta@gmail.com>
2025-03-24 14:20:19 +0100

    [UPD] made the function for selecting the name with the same initial letter

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,7 +4,10 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function selectLetter(arr, letter){
+    let letterArr = [];
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
